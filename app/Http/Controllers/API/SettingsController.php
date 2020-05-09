7b7a28c3d15b506dc08c2ced7ae88ecca2224778
Dagbouj-Hatem <dagboujhatem@gmail.com>
2020-05-09 14:05:14 +0100

    users settings and update info ( added api docs )

--- app/Http/Controllers/API/SettingsController.php
+++ app/Http/Controllers/API/SettingsController.php
@@ -22,7 +22,46 @@ class SettingsController extends AppBaseController
     }
 
 
-    // get user info by email
+
+    /**
+     * @param int $id
+     * @return Response
+     *
+     * @SWG\Get(
+     *      path="/users/getInfos/{email}",
+     *      summary="Display the specified User",
+     *      tags={"User"},
+     *      description="Get User",
+     *      produces={"application/json"},
+     *      security = {{"Bearer": {}}},
+     *      @SWG\Parameter(
+     *          name="email",
+     *          description="email of User",
+     *          type="string",
+     *          required=true,
+     *          in="path"
+     *      ),
+     *      @SWG\Response(
+     *          response=200,
+     *          description="successful operation",
+     *          @SWG\Schema(
+     *              type="object",
+     *              @SWG\Property(
+     *                  property="success",
+     *                  type="boolean"
+     *              ),
+     *              @SWG\Property(
+     *                  property="data",
+     *                  ref="#/definitions/User"
+     *              ),
+     *              @SWG\Property(
+     *                  property="message",
+     *                  type="string"
+     *              )
+     *          )
+     *      )
+     * )
+     */
     public function getUserInfoByEmailAddress($email)
     {
         /** @var User $user */
@@ -38,7 +77,54 @@ class SettingsController extends AppBaseController
         return $this->sendResponse($user->toArray(), 'L\'utilisateur a été récupéré avec succès.');
     }
 
-    // save setting
+
+    /**
+     * @param int $id
+     * @param UpdateUserAPIRequest $request
+     * @return Response
+     *
+     * @SWG\Put(
+     *      path="/users/saveInfos/{id}",
+     *      summary="Update the specified User in storage",
+     *      tags={"User"},
+     *      description="Update User",
+     *      produces={"application/json"},
+     *      security = {{"Bearer": {}}},
+     *      @SWG\Parameter(
+     *          name="id",
+     *          description="id of User",
+     *          type="integer",
+     *          required=true,
+     *          in="path"
+     *      ),
+     *      @SWG\Parameter(
+     *          name="body",
+     *          in="body",
+     *          description="User that should be updated",
+     *          required=false,
+     *          @SWG\Schema(ref="#/definitions/User")
+     *      ),
+     *      @SWG\Response(
+     *          response=200,
+     *          description="successful operation",
+     *          @SWG\Schema(
+     *              type="object",
+     *              @SWG\Property(
+     *                  property="success",
+     *                  type="boolean"
+     *              ),
+     *              @SWG\Property(
+     *                  property="data",
+     *                  ref="#/definitions/User"
+     *              ),
+     *              @SWG\Property(
+     *                  property="message",
+     *                  type="string"
+     *              )
+     *          )
+     *      )
+     * )
+     */
     public function saveSettings($id, UpdateUserAPIRequest $request)
     {
         $input = $request->all();
